820de105260caee8c671f1721ac29e7d4c10d00d
polo <lg550055@hotmail.com>
2022-05-09 19:16:18 -0500

    Adds requirements.txt

--- api/hello.py
+++ api/hello.py
@@ -1,10 +1,17 @@
 from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
+
 
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        message = "Howdy"
-        self.wfile.write(message.encode())
-        return
+  def do_GET(self):
+    url_components = parse.urlsplit(self.path)
+    query = parse.parse_qsl(url_components)
+    dic = dict(query)
+    
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    message = "Howdy"
+    self.wfile.write(message.encode())
+    return
